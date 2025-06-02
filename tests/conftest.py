d8820a9359b5eec6c63d6e49dbd4dc41e0647c16
Viacheslav Suchkov <iexploudi@gmail.com>
2025-06-02 08:11:17 +0400

    Генерация данных и заполнение полей

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+import random
 
 
 @pytest.fixture(scope="session")
@@ -22,3 +23,68 @@ def init_browser():
 def site(init_browser):
 	open_browser = browser.open("https://demoqa.com/automation-practice-form")
 	return open_browser
+
+
+def get_state():
+	states = ["NCR", "Uttar Pradesh", "Haryana", "Rajasthan"]
+	return states[random.randint(0, len(states) - 1)]
+
+def get_city(state):
+	city_dict = {
+		"NCR": ("Delhi", "Gurgaon", "Noida"),
+		"Uttar Pradesh": ("Agra", "Lucknow", "Merrut"),
+		"Haryana": ("Karnal", "Panipat"),
+		"Rajasthan": ("Jaipur", "Jaiselmer")
+    }
+
+	city = city_dict[state]
+	return {state: city[random.randint(0, len(city) - 1)]}
+
+
+def get_gender():
+	return random.choice(["Male", "Female", "Other"])
+
+
+def get_month():
+	return random.choice(["January", "February", "March",
+						  "April", "May", "June", "July",
+						  "August", "September", "October",
+						  "November", "December"])
+
+
+def get_year():
+	return random.randint(1900, 2100)
+
+@pytest.fixture()
+def get_user_date():
+	gender = get_gender()
+	location = get_city(get_state())
+	birthday = "01 " + get_month() + " " + str(get_year())
+
+	if gender == "Male":
+		return {"firstName": "Игорь",
+				"lastName": "Тестов",
+				"userEmail": "test@mail.ru",
+				"gender": "Male",
+				"birthDay": birthday,
+				"mobile_phone": "8989567453",
+				"location": location
+				}
+	elif gender == "Female":
+		return {"firstName": "Лара",
+				"lastName": "Тестова",
+				"userEmail": "lara_t@mail.ru",
+				"gender": "Female",
+				"birthDay": birthday,
+				"mobile_phone": "8939777453",
+				"location": location
+				}
+	else:
+		return {"firstName": "Пользователь",
+				"lastName": "Неопределён",
+				"userEmail": "secret_g@mail.ru",
+				"gender": "Other",
+				"birthDay": birthday,
+				"mobile_phone": "8939777666",
+				"location": location
+				}
--- tests/test_search_from_form.py
+++ tests/test_search_from_form.py
@@ -1,13 +1,36 @@
-import selene
+from selene import be, have
 from selenium.webdriver.common.keys import Keys
+import os
 
 
-def test_search(site):
-	site.element('[id=firstName]').type("start")
-	site.element('[id=lastName]').type("start2")
-	site.element('[id=userEmail]').type("start3")
-	site.element("input[value='Male'] + label").click()
-	site.element('[id=userNumber]').type("7689334332")
-	site.element('[id=dateOfBirthInput]').press(Keys.SHIFT + Keys.HOME).press(Keys.NUMPAD1).set_value("0.10.1999")
-	site.element('input[id = subjectsInput]').type("Com").press_enter()
-	site.element('input[id = subjectsInput]').type("Eng").press_enter()
+def test_search(site, get_user_date):
+	site.execute_script("$('#fixedban').remove()")
+	site.execute_script("$('footer').remove()")
+	location = list(get_user_date["location"].items())[0]
+
+	site.element('[id=firstName]').type(get_user_date["firstName"])
+	site.element('[id=lastName]').type(get_user_date["lastName"])
+	site.element('[id=userEmail]').type(get_user_date["userEmail"])
+	site.element(f"input[value='{get_user_date['gender']}'] + label").click()
+	site.element('[id=userNumber]').type(get_user_date['mobile_phone'])
+	# site.element('[id=dateOfBirthInput]').press(Keys.SHIFT + Keys.HOME).press(Keys.NUMPAD0).set_value(get_user_date['birthDay'])
+
+	# Выбор даты рождения
+	site.element('[id=dateOfBirthInput]').click()
+	# Выбор года
+	site.element(f"[value='{get_user_date['birthDay'].split()[2]}']").click()
+	# Выбор месяца:
+	site.element(".react-datepicker__month-select").element(f"//option[text()='{get_user_date['birthDay'].split()[1]}']").click()
+	# Выбор дня:
+	site.element(f".react-datepicker__day--0{get_user_date['birthDay'].split()[0]}").click()
+
+	site.element('input[id = subjectsInput]').type("Com").press_enter().type("Eng").press_enter()
+	site.element('input[id=hobbies-checkbox-2] ~ label').click()
+	site.element("#uploadPicture").send_keys(os.path.abspath("../files/picture.jpg"))
+	site.element("#currentAddress").type(f"{site.driver.current_url}")
+	site.element('#state input').type(location[0]).press_enter()
+	site.element('#city input').type(location[1]).press_enter()
+	site.element('#submit').click()
+
+	site.element(".table-responsive").all("td").should(have.exact_text(get_user_date["firstName"]))
+	print(1)
