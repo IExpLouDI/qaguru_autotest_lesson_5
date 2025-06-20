e6bacac534a66b83e5b1401ec917ff1355f0beee
Viacheslav Suchkov <iexploudi@gmail.com>
2025-06-20 07:37:53 +0400

    RegistrationPage select_birthday

--- model/pages/registration_page.py
+++ model/pages/registration_page.py
@@ -19,5 +19,23 @@ class RegistrationPage:
 		browser.element('[id=userEmail]').type(value)
 		return self
 
-	def
+	def select_gender(self, value):
+		browser.element(f"input[value='{value}'] + label").click()
+		return self
 
+	def fill_user_mobile_number(self, value):
+		browser.element('[id=userNumber]').type(value)
+		return self
+
+	def select_birthday(self, year, month, day):
+		# Выбор даты рождения
+		browser.element('[id=dateOfBirthInput]').click()
+		# Выбор года
+		browser.element(f"[value='{year}']").click()
+		# Выбор месяца:
+		browser.element(".react-datepicker__month-select").element(
+			f"//option[text()='{month}']").click()
+		# Выбор дня:
+		browser.element(f".react-datepicker__day--0{day}").click()
+
+		return self
